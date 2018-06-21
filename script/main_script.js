7f8a457cb18f0b7ccbaaef295c2598be5953b3e7
unknown <austinvdschultz@gmail.com>
2018-06-21 13:46:09 -0700

    Change script and css to model framework for final site responsiveness 	To do: update script for closing content.

--- script/main_script.js
+++ script/main_script.js
@@ -1,2 +1,21 @@
-
 console.log("Script loaded");
+
+var allboxes= document.querySelector(".container");
+allboxes.addEventListener("click", boxClicked, false);
+
+const boxIds= ['content1', 'content2', 'content3', 'content4', 'content5', 'content6']
+
+function boxClicked(e){
+	changeBox(e.target.id)
+}
+
+function changeBox(boxID){
+	boxIds.forEach(Id=> {
+		if (Id != boxID){
+			document.getElementById(Id).style.visibility ='hidden';
+			document.getElementById(Id).style.position ='absolute';
+		}
+	});
+	document.getElementById(boxID).className += " ClickedBox"
+	console.log(document.getElementById(boxID).className)
+}
