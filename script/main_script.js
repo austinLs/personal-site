bb9100e71409dcac69131c5cb5c9d5bc14b47bf4
unknown <austinvdschultz@gmail.com>
2018-09-17 21:00:42 -0700

    Update script for final functionality

--- script/main_script.js
+++ script/main_script.js
@@ -1,21 +1,45 @@
-console.log("Script loaded");
 
-var allboxes= document.querySelector(".container");
-allboxes.addEventListener("click", boxClicked, false);
+var allboxes= document.querySelectorAll(".content-box");
+allboxes.forEach(elem => {elem.addEventListener("click", boxClicked, false)});
 
-const boxIds= ['content1', 'content2', 'content3', 'content4', 'content5', 'content6']
+const boxIds= ["content1", "content2", "content3", "content4", "content5", "content6"]
 
 function boxClicked(e){
 	changeBox(e.target.id)
 }
 
-function changeBox(boxID){
+function changeBox(boxId){
 	boxIds.forEach(Id=> {
-		if (Id != boxID){
-			document.getElementById(Id).style.visibility ='hidden';
-			document.getElementById(Id).style.position ='absolute';
+		// Hide all boxes that aren't clicked
+		if (Id != boxId){
+			document.getElementById(Id).style.visibility ="hidden";
+			document.getElementById(Id).style.position ="absolute";
 		}
 	});
-	document.getElementById(boxID).className += " ClickedBox"
-	console.log(document.getElementById(boxID).className)
+	//Modify class of clicked box to expand
+	let selectedBox = document.getElementById(boxId);
+	if(selectedBox.className == "content-box"){
+			selectedBox.className += " clicked-box"
+			closeButton(selectedBox);
+		}
+}
+
+function resetBoxes(){
+	boxIds.forEach(Id=> {
+		// Reset all boxes
+			document.getElementById(Id).style.visibility ="visible";
+			document.getElementById(Id).style.position ="relative";
+			document.getElementById(Id).className ="content-box";
+	});
+	const closeButt = document.querySelector(".close-button")
+	if(closeButt)
+		document.querySelector(".container").removeChild(closeButt);
+}
+
+function closeButton(){
+	const closeButt = document.createElement("BUTTON");
+	closeButt.innerText = "X";
+	closeButt.className = "close-button";
+	closeButt.onclick = resetBoxes;
+	document.querySelector(".container").appendChild(closeButt);
 }
